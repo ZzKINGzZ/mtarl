9fa0afb7f692991d7eb862550c5388a1b61ffcc6
Werni <werni@mta-sa.org>
2013-05-15 18:16:58 +0000

    Fixing a problem while create a new account

--- require/mysql/mtarl_clean.sql
+++ require/mysql/mtarl_clean.sql
@@ -540,6 +540,7 @@ CREATE TABLE IF NOT EXISTS `players` (
   `forumid` int(10) NOT NULL DEFAULT '-1',
   `activeClothes` varchar(255) NOT NULL DEFAULT 'return {{},}--|',
   `registerTime` int(25) NOT NULL DEFAULT '0',
+  `wanted` int(11) NOT NULL DEFAULT '0',
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=2 ;
 
